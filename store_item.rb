d5d1578be6df1a9294fdc8321f4982c71ac9b49e
Alex Via <alexandervia@gmail.com>
2022-07-19 13:36:59 -0500

    created a store for cars, denoted with hashes

---
+++ store_item.rb
@@ -0,0 +1,22 @@
+car1 = {
+  :make => "Honda",
+  :model => "Fit",
+  :color => "Silver",
+  :price => 15000
+}
+
+car2 = {
+  :make => "Toyota",
+  :model => "Camry",
+  :color=> "Black",
+  :price => 12000
+}
+
+car3 = {
+  make: "Chevy",
+  model: "Venture",
+  color: "Red",
+  price: 10000
+}
+
+puts "The first car is a #{car1[:color]} #{car1[:make]} #{car1[:model]} and costs $#{car1[:price]}."
